5fa9e38d5b2a5d10b49ae52bcf4bc34ba9fb735a
Wang <wan333@csiro.au>
2019-12-02 17:14:34 +1100

    find the error in curve fitting

--- scripts/fitting/curve_fitting_15P_ver2.R
+++ scripts/fitting/curve_fitting_15P_ver2.R
@@ -16,36 +16,9 @@ data_15P_cal_HE_outlier_deleted <- read_csv("data/tidydata/data_15P_cal_HE_outli
 #                                             ** extrat parameters **
 
 
-# built the model and put it into the loop
-find_paramters <- function(data_15P_cal_HE_outlier_deleted){ # creat a function called find_parameters
-  
-  # remove the missing value
-  
-  hydro_data <- data_15P_cal_HE_outlier_deleted %>% 
-    filter(!is.na(HE)) 
-  
-  # built the model with some guessing values of the unknown parameters
-  
-  model <- nls(formula = HE ~ Xinf*(1-exp(-k*Time**(1-h))), # using the Weibull function 
-               data = hydro_data,
-               algorithm = "port",
-               start = list(Xinf = 73,
-                            k = 0.003,
-                            h = 0.0005),
-               # lower = list(Xinf = 0,
-               #       k = 0,
-               #      h = 0),
-               control = list(warnOnly = TRUE))
-  
-  # turn the results into a tidy tibble
-  
-  result <- tidy(model) %>% 
-    select(term, estimate) %>% # extract the values of these pamameters
-    spread(term, estimate) # split into three separate columns
-  result # print out the result
-}
+# let's use two functions to generate the fitted model and the estimated parameters
 
-fit_model <- function(data_15P_cal_HE_outlier_deleted){ # creat a function called find_parameters
+fit_model <- function(data_15P_cal_HE_outlier_deleted){ # creat a function called fit_model
   
   # remove the missing value
   
@@ -56,59 +29,47 @@ fit_model <- function(data_15P_cal_HE_outlier_deleted){ # creat a function calle
   
   model <- nls(formula = HE ~ Xinf*(1-exp(-k*Time**(1-h))), # using the Weibull function 
                data = hydro_data,
-               algorithm = "port",
+               algorithm = "port", # add this if setting the constrains 
                start = list(Xinf = 73,
                             k = 0.003,
                             h = 0.0005),
-               # lower = list(Xinf = 0,
-                     #       k = 0,
-                      #      h = 0),
-               control = list(warnOnly = TRUE))
+               lower = list(Xinf = 0,
+                            k = 0,
+                            h = -0.5),
+               control = list(warnOnly = TRUE)) # change the setting, give us just the waining messages instead of error messages
   
-  # turn the results into a tidy tibble
-  
-  result <- tidy(model) %>% 
-    select(term, estimate) %>% # extract the values of these pamameters
-    spread(term, estimate) # split into three separate columns
-  result # print out the result
+return(model) # give back the model
+
 }
 
 
-find_paramters <- function(data_15P_cal_HE_outlier_deleted){ # creat a function called find_parameters
-  
-  # remove the missing value
-  
-  hydro_data <- data_15P_cal_HE_outlier_deleted %>% 
-    filter(!is.na(HE)) 
-  
-  # built the model with some guessing values of the unknown parameters
-  
-  model <- nls(formula = HE ~ Xinf*(1-exp(-k*Time**(1-h))), # using the Weibull function 
-               data = hydro_data,
-               algorithm = "port",
-               start = list(Xinf = 73,
-                            k = 0.003,
-                            h = 0.0005),
-               # lower = list(Xinf = 0,
-               #       k = 0,
-               #      h = 0),
-               control = list(warnOnly = TRUE))
-  
-  # turn the results into a tidy tibble
+find_paramters <- function(model){ # creat a function called find_parameters
   
   result <- tidy(model) %>% 
     select(term, estimate) %>% # extract the values of these pamameters
     spread(term, estimate) # split into three separate columns
-  result # print out the result
+  result # give back the result
 }
 
 
 # extract the parameters 
 
-parameters <- hydro_data %>% 
+model_list <- hydro_data %>% 
   split(.$Well) %>% # split the initial dataset into 182 subsets by sample
-  map_df(find_paramters) # error here, can not generate the estimateed parmaters
+  map(fit_model) # error here, can not generate the estimateed parmaters
  # may be not gussing the right values for the k, h and Xinf
+
+get_isconv <- function(x)x$convInfo$isConv
+
+# sum(map_chr(model_list, class) != "nls", na.rm = TRUE)
+
+isconv <- model_list %>% 
+  map_lgl(.f = get_isconv) # give back a logical vector 
+
+names(isconv)[isconv == FALSE]
+
+parameters <- model_list %>% 
+  map_df(find_paramters) # error here, can not generate the estimateed parmaters
 
 # not matter we set the constraints or not, the estimate parameters can not be generated 
 
