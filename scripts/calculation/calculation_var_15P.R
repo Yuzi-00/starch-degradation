4459ff9efff49254dfd3018790d9098bb6a9a7e3
Wang <wan333@csiro.au>
2019-12-05 16:38:40 +1100

    change the working dataset

--- scripts/calculation/calculation_var_15P.R
+++ scripts/calculation/calculation_var_15P.R
@@ -5,9 +5,9 @@ library(tidyverse)
 #                              ** calculate the mean, sd and cov **
 
 
-data_15P_cal_HE_outlier_deleted <- read_csv("data/tidydata/data_15P_cal_HE_outlier_deleted.csv")
+data_15P_cal_HE_outlier_replaced <- read_csv("data/tidydata/data_15P_cal_HE_outlier_replaced.csv")
 
-variation <- data_15P_cal_HE_outlier_deleted %>% 
+variation <- data_15P_cal_HE_outlier_replaced %>% 
   group_by(Sample, Time) %>% 
   summarise(Mean_HE = mean(HE, na.rm = TRUE), 
             Sd_HE = sd(HE, na.rm = TRUE), 
@@ -19,7 +19,7 @@ write_csv(variation, "analysis/variation_15P.csv")
 
 # combine the variation with the "data_15P_cal_HE_outlier_deleted" dataset
 
-data_15P_cal_var <- left_join(data_15P_cal_HE_outlier_deleted, variation)
+data_15P_cal_var <- left_join(data_15P_cal_HE_outlier_replaced, variation)
 
 # save the joined dataset
 
