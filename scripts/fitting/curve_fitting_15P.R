43de9f043bcc7974087617467b1c19fa2cd5ed88
Wang <wan333@csiro.au>
2019-12-05 13:36:42 +1100

    residual plot

--- scripts/fitting/curve_fitting_15P.R
+++ scripts/fitting/curve_fitting_15P.R
@@ -10,20 +10,20 @@ library(tidyverse)
 library(broom)
 
 
-#                                             ** extrat parameters ** 
+#                                             ** modelling with control samples ** 
 
 
 # read in the dataset
 
-data_15P_cal_HE_outlier_deleted <- read_csv("data/tidydata/data_15P_cal_HE_outlier_deleted.csv")
+data_15P_cal_HE_outlier_replaced <- read_csv("data/tidydata/data_15P_cal_HE_outlier_replaced.csv")
 
 # built the model and put it into the loop
 
-find_paramters <- function(data_15P_cal_HE_outlier_deleted){ # creat a function called find_parameters
+find_paramters <- function(data_15P_cal_HE_outlier_replaced){ # creat a function called find_parameters
   
   # remove the missing value
   
-  hydro_data <- data_15P_cal_HE_outlier_deleted %>% 
+  hydro_data <- data_15P_cal_HE_outlier_replaced %>% 
     filter(!is.na(HE)) 
   
     # built the model with some guessing values of the unknown parameters
@@ -54,6 +54,147 @@ parameters <- hydro_data %>%
 # if we want to check the type of the data 
 
 class(parameters) # we can see that it's a data frame (a tibble)
+
+# check the model 
+
+plot(residuals(model)) # this gives us the residuals in order, don't use this one
+
+plot(model) # this is the residuals against fitted values, the variance is quite big
+# let's remove the control samples and see if it's better
+
+
+#                                             ** modelling without control samples ** 
+
+
+# remove the control samples 
+
+data_control_removed <- data_15P_cal_HE_outlier_replaced %>% 
+  filter(Sample != "C+" & Sample != "C-")
+
+# built the model and put it into the loop
+
+find_paramters <- function(data_control_removed){ # creat a function called find_parameters
+  
+  # remove the missing value
+  
+  hydro_data <- data_control_removed %>% 
+    filter(!is.na(HE)) 
+  
+  # built the model with some guessing values of the unknown parameters
+  
+  model <- nls(formula = HE ~ Xinf*(1-exp(-k*Time**(1-h))), # using the Weibull function 
+               data = hydro_data,
+               start = list(Xinf = 100,
+                            k = 0.001,
+                            h = 0.001))
+  
+  # turn the results into a tidy tibble
+  
+  result <- tidy(model) %>% # the tidy function comes from the broom package
+    select(term, estimate) %>% # extract the values of these pamameters
+    spread(term, estimate) # split into three separate columns
+  result # print out the result
+}
+
+# extract the parameters 
+
+parameters <- hydro_data %>% 
+  split(.$Sample) %>% # split the initial dataset into 182 subsets by sample
+  map_df(find_paramters) 
+# the map function transform its input by applying the function find_parameters 
+# and save the outputs all together into a list
+# while the map_df function will do the same but save the outputs all together into a data frame
+
+# if we want to check the type of the data 
+
+class(parameters) # we can see that it's a data frame (a tibble)
+
+# check the model 
+
+plot(residuals(model)) # this gives us the residuals in order, don't use this one
+
+plot(model) # the variance is reduced, but still...
+
+# have to find the two points below -5 according to the plot
+
+# add a column for residuals 
+
+hydro_data$residuals = (residuals(model))
+
+# find the minimum value of the residuals
+
+which.min(hydro_data$residuals) # it's the 259
+
+# find the corresponding sample 
+
+hydro_data$Sample[259] # it's the sample 92, which is not surprising 
+
+
+#                                             ** modelling without sample 92 ** 
+
+
+# remove the control samples 
+
+data_92_removed <- data_control_removed %>% 
+  filter(Sample != "92")
+
+# built the model and put it into the loop
+
+find_paramters <- function(data_92_removed){ # creat a function called find_parameters
+  
+  # remove the missing value
+  
+  hydro_data <- data_92_removed %>% 
+    filter(!is.na(HE)) 
+  
+  # built the model with some guessing values of the unknown parameters
+  
+  model <- nls(formula = HE ~ Xinf*(1-exp(-k*Time**(1-h))), # using the Weibull function 
+               data = hydro_data,
+               start = list(Xinf = 100,
+                            k = 0.001,
+                            h = 0.001))
+  
+  # turn the results into a tidy tibble
+  
+  result <- tidy(model) %>% # the tidy function comes from the broom package
+    select(term, estimate) %>% # extract the values of these pamameters
+    spread(term, estimate) # split into three separate columns
+  result # print out the result
+}
+
+# extract the parameters 
+
+parameters <- hydro_data %>% 
+  split(.$Sample) %>% # split the initial dataset into 182 subsets by sample
+  map_df(find_paramters) 
+# the map function transform its input by applying the function find_parameters 
+# and save the outputs all together into a list
+# while the map_df function will do the same but save the outputs all together into a data frame
+
+# if we want to check the type of the data 
+
+class(parameters) # we can see that it's a data frame (a tibble)
+
+# check the model 
+
+plot(residuals(model)) # this gives us the residuals in order, don't use this one
+
+plot(model) # the variance is reduced, but still...
+
+# have to find the two points below -5 according to the plot
+
+# add a column for residuals 
+
+hydro_data$residuals = (residuals(model))
+
+# find the minimum value of the residuals
+
+which.min(hydro_data$residuals) # it's the 259
+
+# find the corresponding sample 
+
+hydro_data$Sample[259] # it's the sample 92, which is not surprising 
 
 
 #                    ** add the sample name to the parameter list ** 
