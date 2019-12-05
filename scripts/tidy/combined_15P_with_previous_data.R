5a49466a2b2c7aa5e27b0e46a6519dbe78e845c1
Wang <wan333@csiro.au>
2019-12-05 14:11:52 +1100

    add RVA and CLD data

--- scripts/tidy/combined_15P_with_previous_data.R
+++ scripts/tidy/combined_15P_with_previous_data.R
@@ -35,10 +35,49 @@ HE_amy_size_15P <- left_join(data_15P_HE_amy, master_size)
 write_csv(HE_amy_size_15P, "data/tidydata/HE_amy_size_15P.csv")
 
 
-#                                                             ** add fitted parameters (Weibull, fit for each sample) **
+#                               ** add fitted parameters (Weibull, fit for each sample) **
 
 parameters <- read_csv("analysis/fitted_Weibull_parameters_15P.csv")
 
 HE_amy_size_para_15P <- left_join(HE_amy_size_15P, parameters)
 
 write_csv(HE_amy_size_para_15P, "results/HE_amy_size_para_15P.csv")
+
+
+
+
+#                                               ** add RVA data **
+
+
+
+
+# import the RVA dataset
+
+RVA_data <- read_csv("data/tidydata/previous_data/RVA_tidy.csv")
+
+# merge the RVA data into this dataset
+
+joined_15P <- left_join(HE_amy_size_para_15P, RVA_data)
+
+# write out the new data frame
+
+write_csv(joined_15P, "data/tidydata/joined_15P.csv")
+
+
+
+
+#                                                ** add CLD data **
+
+# import the CLD dataset
+
+CLD_data <- read_csv("data/tidydata/previous_data/CLD_tidy.csv") %>% 
+  rename(ID = id) ## change the column name to be consistent with other datasets
+
+# combien the CLD data with the previous joined dataset
+
+joined_15P_update <- left_join(joined_15P, CLD_data)
+
+# write out the new joined dataset
+
+write_csv(joined_15P_update, "data/tidydata/joined_15p_update.csv")
+
