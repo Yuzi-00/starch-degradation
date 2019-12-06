040a29e9781cab4d25ad9c00235e483a67c190ec
Wang <wan333@csiro.au>
2019-12-06 16:10:53 +1100

    tidy amylase dataset

---
+++ scripts/previous-data/amylase_tidy.R
@@ -0,0 +1,20 @@
+
+library(tidyverse)
+
+library(readxl)
+
+# import the raw data
+
+amylase <- read_xls("C:/Users/WAN333/Documents/Thesis/Thesis infomation/MAGIC population/Data_MAGIC Population/alpha amylase assays 24-01-11.xls",
+                    sheet = "total data",
+                    range = "A1:D753")
+
+# remove the empty column and rename the column
+
+amylase <- amylase %>% 
+  select(-'...2') %>% 
+  rename(ID = '...1')
+
+# save the tidy dataset
+
+write_csv(amylase, "data/tidydata/previous_data/amylase_tidy.csv")
