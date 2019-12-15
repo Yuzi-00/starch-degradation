1a880764b0053019df8b8111da7364cb0961e8f4
Wang <wan333@csiro.au>
2019-12-15 21:31:24 +1100

    clean up

--- scripts/plot/fitted_HE_plot_15P_ver2.R
+++ scripts/plot/fitted_HE_plot_15P_ver2.R
@@ -165,19 +165,19 @@ dev.off() # stop sending plot to the pdf file
 
 # try using the plot() function instead of ggplot()
 
+# read in the dataset
+
+data_15P <- read_csv("data/tidydata/data_15P_cal_HE_outlier_replaced.csv")
+
+# remove the NAs
 
 data_15P <- data_15P %>%
   filter(!(is.na(HE)))
 
-# import the estimated parameters 
+# creat a loop to send all the plots into a pdf file
 
-para <- read_csv("analysis/fitted_weibull_parameters_for_replicates.csv")
-
-#
-
-pdf(file = "figures/degradability_individual plot_fit_replicate.pdf") # creating a pdf file and senting all the plot below to this file
+pdf(file = "figures/degradability_individual plot_fit_replicates.pdf") # creating a pdf file and senting all the plot below to this file
 for(i in unique(data_15P$Well)){ # i stands for each item within this dataset
-  # unique() can show all the Sample names here whithin the mean_HE_6P dataset 
   
   model_x <- data_15P %>% 
     filter(Well == i) %>% 
@@ -191,14 +191,13 @@ for(i in unique(data_15P$Well)){ # i stands for each item within this dataset
                               k = 0))
                  # control = list(warnOnly = TRUE)
   
-  
   data_15P %>% 
     filter(Well == i) %>% # pipe this to the first argument on the right side 
     # here, the first argument of ggplot is the data, otherwise, we have to type ggplot(data = .)
     # to let it pipe to this argument
     select(Time, HE) %>% 
-    plot(ylim = c(0, 100), main = i) # set the range of the y axis up to 100
-    
+    plot(ylim = c(0, 100), main = i)  # set the range of the y axis up to 100
+
   ## creat a data frame that contains 100 time points
   
   new.data <- data.frame(Time = seq(0, 1800, len = 100))
@@ -206,7 +205,7 @@ for(i in unique(data_15P$Well)){ # i stands for each item within this dataset
   ## draw the new line (fitted line)
   
   lines(new.data$Time, predict(model_x, newdata = new.data), col='red')  
-  
+
 } 
 
 dev.off() # stop sending plot to the pdf file
