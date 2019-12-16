2894c8da5e6dccd6c7fae0e7b9926df9b652dd94
Wang <wan333@csiro.au>
2019-12-16 17:06:02 +1100

    draft

--- scripts/anova.R
+++ scripts/anova.R
@@ -1,18 +1,17 @@
+
+library(tidyverse)
+
+# import the dataset 
+
 total <- read_csv("data/tidydata/joined_15P_update.csv")
 
-selected <- total %>% 
-  filter(Sample %in% c("129", "143", "222")) %>% 
-  filter(Time == 1800) %>% 
-  filter(Well != "13_A_11")
+# calculate the mean value
 
-sum_selected <- selected %>% 
+total %>% 
   group_by(Sample, Time) %>%
-  summarise(mean_HE = mean(Hydro_extent, na.rm = T),
-            mean_amy = mean(Amylose_content, na.rm = T),
-            sd_HE = sd(Hydro_extent, na.rm = T),
-            sd_amy = sd(Amylose_content, na.rm = T))
+  summarise(mean_HE = mean(Hydro_extent, na.rm = T))
 
-selected %>% 
+total %>% 
   filter(Time == 1800) %>% 
   ggplot(aes(x = Sample,
              y = Hydro_extent,
@@ -20,16 +19,10 @@ selected %>%
   geom_boxplot() +
   geom_jitter()
 
-anova <- aov(data = selected, Hydro_extent~Sample)
+anova <- aov(data = total, Hydro_extent~Sample)
 summary(anova)
 
-TukeyHSD(anova)
+x <- TukeyHSD(anova)
 
 
-total["Amylose_content"][is.na(total["Amylose_content"])] = 100
 
-total$Amylose_content[is.na(total$Sample == "C+")] = 100
-
-x <- total %>% 
-  mutate(Amylose_content=ifelse(Sample=="C+",100,Amylose_content),
-         Amylose_content=ifelse(Sample=="C-",72.4,Amylose_content))
