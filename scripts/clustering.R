a61c8cc20fe56799e89648aebf21fdebf5528338
王雨孜 <wangyuzi@wangyuzideMacBook-Pro.local>
2020-03-16 11:45:43 +0800

    more detailed clustering

--- scripts/clustering.R
+++ scripts/clustering.R
@@ -1,6 +1,16 @@
 
 library(tidyverse)
 
+# install.packages("clValid")
+
+# install.packages("cluster")
+
+# install.packages('dendextend', dependencies = TRUE) 
+
+library(dendextend)
+
+library(clValid)
+
 # import the dataset 
 
 total_new <- read_csv("analysis/total_new.csv")
@@ -8,8 +18,9 @@ total_new <- read_csv("analysis/total_new.csv")
 # select the columns 
 
 subdata <- total_new %>% 
-  select(ID, Amylose_content, SSA, Surface_weighted_mean, D1, D5, D9, k, Xinf, mean_Peak, mean_Trough,
-         mean_Final, mean_PastingTemp, low_dp, medium_dp, medium_high_dp, high_dp, mean_amylase, h)
+  select(ID, Amylose_content, SSA, Surface_weighted_mean, D1, D5, D9, k, 
+         Xinf, mean_Peak, mean_Trough, mean_Final, mean_PastingTemp, low_dp, 
+         medium_dp, medium_high_dp, high_dp, mean_amylase, h)
 
 # remove the NAs
 
@@ -26,21 +37,73 @@ subdata_num <- subdata_red %>%
 
 subdata_nor <- scale(subdata_num)
 
-# calculate the distance 
+## notice the mean of all the columns is 0 and the standard deviation is 1 
+## after scaling.
 
-distance <- dist(subdata_nor)
+# build the distance matrix  
+
+distance <- dist(subdata_nor, method = 'euclidean')
+
+## here we use the euclidean distance method as all the values are continuous
+## numerical values.
 
 # dentrogram
 
-hc <- hclust(distance)
+hc <- hclust(distance, method = 'average')
+
+## at this point, we should decide which linkage method we want to use for
+## the hierarchical clustering. We can try all kinds of linkage methods and 
+## later decide on which one performed better. Here we proceed with average 
+## linkage method.
+
+# plot
 
 plot(hc)
 
-#########################################################################################
+## now we can cut the dendrogram in order to create the desired number of 
+## clusters. Here probably three groups as can be seen from the dendrogram
 
-# grouping
+# cut the tree and put them into different clusters 
 
-results <- kmeans(subdata_nor, 5) # choose the number of groups
+rect.hclust(hc, k = 2, border = 2:6)
+
+abline(h = 10, col = 'red', lty = 'dashed')
+
+# another way to color the dendrogram
+
+hc_dend <- as.dendrogram(hc)
+
+hc_col_dend <- color_branches(hc_dend, h = 10)
+
+plot(hc_col_dend)
+
+# calculate how many observations were assigned in each cluster
+
+cut_hc <- cutree(hc, k = 2)
+
+subdata_cl <- mutate(subdata_num, cluster = cut_hc)
+
+count(subdata_cl,cluster) # 608 vs 6
+
+## is it a good clustering as one of the group is too small ? 
+
+# evaluate the trend between two features based on the clustering
+
+ggplot(subdata_cl, aes(x = SSA, y = k, color = factor(cluster))) + geom_point()
+
+## did not find any trend yet based on the clustering 
+
+# calculte the dunn's index
+
+dunn(distance, cut_hc) # 0.603834
+
+## Dunn's index is the ratio between the minimum inter-cluster distances to the maximum 
+## intra-cluster diameter. The diameter of a cluster is the distance between its two furthermost 
+## points. We should aim for higher dunn's index in order to have well separated and compact clusters
+
+# double check the clustering results
+
+results <- kmeans(subdata_nor, 2) # choose the number of groups
 
 table(subdata_red$ID, results$cluster)
 
