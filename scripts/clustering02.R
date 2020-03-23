135cc95966ede4250887393c5d09036b48bca22f
王雨孜 <wangyuzi@wangyuzideMacBook-Pro.local>
2020-03-23 10:29:49 +0800

    dendrogram for parameters

---
+++ scripts/clustering02.R
@@ -0,0 +1,71 @@
+
+library(tidyverse)
+
+# install.packages("clValid")
+
+# install.packages("cluster")
+
+# install.packages('dendextend', dependencies = TRUE) 
+
+library(dendextend)
+
+library(clValid)
+
+# import the dataset 
+
+total_new <- read_csv("analysis/total_new.csv")
+
+# select the columns 
+
+subdata <- total_new %>% 
+  select(ID, Amylose_content, SSA, Surface_weighted_mean, D1, D5, D9, k, 
+         Xinf, mean_Peak, mean_Trough, mean_Final, mean_PastingTemp, low_dp, 
+         medium_dp, medium_high_dp, high_dp, mean_amylase, h)
+
+# remove the NAs
+
+subdata_red <- subdata %>% 
+  unique() %>% 
+  na.omit()
+
+# remove the Sample column
+
+subdata_num <- subdata_red %>% 
+  select(-ID)
+
+# exchange columns and rows
+
+subdata_transpose = t(subdata_num)
+
+# normalization
+
+subdata_nor <- scale(subdata_transpose)
+
+
+
+## notice the mean of all the columns is 0 and the standard deviation is 1 
+## after scaling.
+
+# build the distance matrix  
+
+distance <- dist(subdata_nor, method = 'euclidean')
+
+## here we use the euclidean distance method as all the values are continuous
+## numerical values.
+
+# dentrogram
+
+hc <- hclust(distance, method = 'average')
+
+## at this point, we should decide which linkage method we want to use for
+## the hierarchical clustering. We can try all kinds of linkage methods and 
+## later decide on which one performed better. Here we proceed with average 
+## linkage method.
+
+# plot
+
+plot(hc)
+
+## now we can cut the dendrogram in order to create the desired number of 
+## clusters. Here probably three groups as can be seen from the dendrogram
+
