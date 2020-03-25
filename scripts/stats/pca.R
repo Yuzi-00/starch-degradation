cd93d1241037e028cc8de750119cc65c0335ffab
王雨孜 <wangyuzi@wangyuzideMacBook-Pro.local>
2020-03-25 22:01:27 +0800

    complete factoextra

--- scripts/stats/pca.R
+++ scripts/stats/pca.R
@@ -141,6 +141,22 @@ fviz_eig(res.pca)
 
 ## it shows the percentage of variances explained by each principal component
 
+fviz_pca_ind(res.pca,
+             col.ind = "cos2", # Color by the quality of representation
+             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+             repel = FALSE     # Avoid text overlapping
+)
+
+fviz_pca_var(res.pca,
+             col.var = "contrib", # Color by contributions to the PC
+             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+             repel = TRUE     # Avoid text overlapping
+)
+
+fviz_pca_biplot(res.pca, repel = FALSE,
+                col.var = "#2E9FDF", # Variables color
+                col.ind = "#696969"  # Individuals color
+)
 
 
 
