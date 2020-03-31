697e2a00b87a610a3b7bf89f9f3c88fc38737a3e
王雨孜 <wangyuzi@wangyuzideMacBook-Pro.local>
2020-03-31 16:25:58 +0800

    plot k and h

---
+++ scripts/plot/kinetics_2D.R
@@ -0,0 +1,17 @@
+
+library(tidyverse)
+
+library(plotly)
+
+# import the dataset
+
+df <- read_csv("analysis/total_new_convert.csv")
+
+# plot
+
+df %>%
+  plot_ly(x = ~h, y = ~k, 
+          alpha = 0.6,
+          color = ~Category, 
+          colors = "Dark2")
+)
