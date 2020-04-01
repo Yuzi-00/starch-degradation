d35e95c57bc4761451c7ad500c1ff731ae4e7d3b
王雨孜 <wangyuzi@192.168.0.108>
2020-04-01 20:51:24 +0800

    change the plot

--- scripts/stats/correlation_h_k.R
+++ scripts/stats/correlation_h_k.R
@@ -219,50 +219,50 @@ plot01 <- test_sample %>%
   stat_cor(label.y = -6)+ #this means at 35th unit in the y axis, the r squared and p value will be shown
   stat_regline_equation(label.y = -7) 
 
-colors <- c("Test sample" = "black", "Positive control" = "red", "Negative control" = "blue")
-
-ggplot() +
+P <- ggplot() +
   geom_point(data = test_sample,
              aes(x = k, 
-                 y = h),
+                 y = h,
+                 color = "Test_sample"),
              alpha = 0.5) +
   geom_smooth(data = test_sample,
               aes(x = k, 
                   y = h),
               method = "lm", se=FALSE, color="black", 
-              formula = y ~ log(x)) +
+              formula = y ~ log(x, base = exp(1))) +
   stat_cor(data = test_sample, aes(x = k, 
                                    y = h),
            label.x = 0.003, label.y = 0.23) + #this means at 35th unit in the y axis, the r squared and p value will be shown
   stat_regline_equation(data = test_sample, aes(x = k, 
                                                 y = h),
-                        formula = y ~ log(x),
+                        formula = y ~ log(x, base = exp(1)),
                         label.x = 0.003, label.y = 0.27) +
   # add positive control
   geom_point(data = pos_control,
              aes(x = k, 
-                 y = h),
+                 y = h,
+                 color = "Pos_control"),
              alpha = 0.5,
-             color = "red") +
+             ) +
   geom_smooth(data = pos_control,
               aes(x = k, 
                   y = h
               ),
               method = "lm", se=FALSE, color="red", 
-              formula = y ~ log(x)) +
+              formula = y ~ log(x, base = exp(1))) +
   stat_cor(data = pos_control, aes(x = k, 
                                    y = h),
            label.x = 0.005, label.y = -0.15,
            color="red") + #this means at 35th unit in the y axis, the r squared and p value will be shown
   stat_regline_equation(data = pos_control, aes(x = k, 
                                                 y = h),
-                        formula = y ~ log(x),
+                        formula = y ~ log(x, base = exp(1)),
                         label.x = 0.005, label.y = -0.1, color="red") +
   # add negative control
   geom_point(data = neg_control,
              aes(x = k, 
                  y = h,
-                 color = "NC"),
+                 color = "Neg_control"),
              alpha = 0.5
              ) +
   geom_smooth(data = neg_control,
@@ -270,7 +270,7 @@ ggplot() +
                   y = h
               ),
               method = "lm", se=FALSE, color="blue", 
-              formula = y ~ log(x)) +
+              formula = y ~ log(x, base = exp(1))) +
   stat_cor(data = neg_control, aes(x = k, 
                                    y = h),
            label.x = 0.01,
@@ -278,10 +278,10 @@ ggplot() +
            color="blue") + #this means at 35th unit in the y axis, the r squared and p value will be shown
   stat_regline_equation(data = neg_control, aes(x = k, 
                                                 y = h),
-                        formula = y ~ log(x),
+                        formula = y ~ log(x, base = exp(1)),
                         label.x = 0.01, label.y = 0.15, color="blue") +
-  scale_colour_manual(name="Line Color",
-                      values=c(PC="red", TS="black", NC = "blue"))
+  scale_colour_manual(name="Sample type",
+                      values=c(Pos_control="red", Test_sample="black", Neg_control = "blue"))
 
 P
 
@@ -289,8 +289,8 @@ P
 
 ggsave("figures/hk_abline.png", 
        plot = P, 
-       width = 17, 
-       height = 12, 
+       width = 25, 
+       height = 20, 
        units = "cm") 
 
   
