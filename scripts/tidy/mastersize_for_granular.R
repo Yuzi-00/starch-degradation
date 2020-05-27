f558443c0254fd59ac3e61d48da6d5bd1a40b6cb
王雨孜 <wangyuzi@wangyuzideMacBook-Pro.local>
2020-05-27 10:58:01 +0800

    tidy the mastersize dataset

--- scripts/tidy/mastersize_for_granular.R
+++ scripts/tidy/mastersize_for_granular.R
@@ -1,29 +1,19 @@
 library(tidyverse)
 
-library(readxl)
+library(janitor)
 
 # read in the raw data
 
-raw_data <- read_xls("data/magic_population/MastersizerMagic.xls",
-                     sheet = "Total data",
-                     range = "A2:DH515")
-
-
-df <- raw_data[-1,] # remove the first empty row
-
-# select the useful columns 
-
-df_new <- df %>%
-  select(-id) %>%
-  select(-(2:11)) %>%
-  rename(sample = "Sample Name")
+raw_data <- read_csv("data/magic_population/MastersizerMagic_granular.csv")
 
 # calculate the mean value for each sample 
 
-df_mean <- df_new %>%
-  group_by(sample) %>%
-  summarise_all(mean)
+df <- raw_data %>%
+  group_by(Sample) %>%
+  summarise_all(mean) 
 
 # save the dataset
 
-write_csv(df_mean, "data/tidydata/mastersize_granular.csv")
+write_csv(df, "data/tidydata/mastersize_granular.csv")
+
+# this dataset needs further select for the useful columns  
