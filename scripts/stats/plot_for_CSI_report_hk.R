6500590e62db5e6769368204eda5a05cc697c980
王雨孜 <wangyuzi@wangyuzideMacBook-Pro.local>
2020-05-29 16:57:48 +0800

    add control samples

--- scripts/stats/plot_for_CSI_report_hk.R
+++ scripts/stats/plot_for_CSI_report_hk.R
@@ -1,7 +1,7 @@
 
 # !!!!!! run script correlation_h_k before this script !!!!!!
 
-# plot the h k model without the control samples
+# plot the h k model with the control samples
 
 library(ggpubr)
 
@@ -22,6 +22,52 @@ P <- ggplot() +
                                                 y = h),
                         formula = y ~ log(x, base = exp(1)),
                         label.x = 0.008, label.y = 0.05, size = 5) +
+  # add positive control
+  geom_point(data = pos_control,
+             aes(x = k, 
+                 y = h,
+                 color = "Pos_control"),
+             alpha = 0.5,
+  ) +
+  geom_smooth(data = pos_control,
+              aes(x = k, 
+                  y = h
+              ),
+              method = "lm", se=FALSE, color="red", 
+              formula = y ~ log(x, base = exp(1))) +
+  stat_cor(data = pos_control, aes(x = k, 
+                                   y = h),
+           label.x = 0.005, label.y = -0.15,
+           color="red") + #this means at 35th unit in the y axis, the r squared and p value will be shown
+  stat_regline_equation(data = pos_control, aes(x = k, 
+                                                y = h),
+                        formula = y ~ log(x, base = exp(1)),
+                        label.x = 0.005, label.y = -0.1, color="red") +
+  # add negative control
+  geom_point(data = neg_control,
+             aes(x = k, 
+                 y = h,
+                 color = "Neg_control"),
+             alpha = 0.5
+  ) +
+  geom_smooth(data = neg_control,
+              aes(x = k, 
+                  y = h
+              ),
+              method = "lm", se=FALSE, color="blue", 
+              formula = y ~ log(x, base = exp(1))) +
+  stat_cor(data = neg_control, aes(x = k, 
+                                   y = h),
+           label.x = 0.01,
+           label.y = 0.1,
+           color="blue") + #this means at 35th unit in the y axis, the r squared and p value will be shown
+  stat_regline_equation(data = neg_control, aes(x = k, 
+                                                y = h),
+                        formula = y ~ log(x, base = exp(1)),
+                        label.x = 0.01, label.y = 0.15, color="blue") +
+  scale_colour_manual(name="Sample type",
+                      values=c(Pos_control="red", Test_sample="black", Neg_control = "blue")) +
+  scale_x_continuous(limits = c(0, 0.02), expand = c(0, 0)) +
   theme(axis.text.x = element_text(color="black", size=15), 
         axis.text.y = element_text(color="black", size=15)) +
   theme(axis.title.x = element_text(size = 15),
