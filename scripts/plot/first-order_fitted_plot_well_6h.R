71e64aa8e9100c68f80c3359f0d09c3320c6369d
王雨孜 <wangyuzi@wangyuzideMacBook-Pro.local>
2020-06-29 14:58:35 +0800

    comparaison first order equation fitting 6h vs 24h

---
+++ scripts/plot/first-order_fitted_plot_well_6h.R
@@ -0,0 +1,92 @@
+
+library(tidyverse)
+
+# read in the dataset
+
+data_15P <- read_csv("data/tidydata/data_15P_cal_HE_outlier_replaced.csv")
+
+# remove the NAs
+
+total <- data_15P %>%
+  filter(!(is.na(HE)))
+
+# creat a loop to store all the plots into a pdf document
+
+pdf(file = "figures/first-order_6h_fit_HE_well_with_T0.pdf") # creating a pdf file and senting all the plot below to this file
+for(i in unique(total$Well)){ # i stands for each item within this dataset
+  # unique() can show all the Sample names here whithin the mean_HE_6P dataset 
+  
+  model_x <- total %>%
+    filter(Well == i) %>%
+    nls(formula = HE ~ Xinf*(1-exp(-k*Time)), 
+        data = . ,
+        algorithm = "port", # add this if setting the constrains 
+        start = list(Xinf = 73, # some guessing values of the parameters
+                     k = 0.003),
+        lower = list(Xinf = 0, # set the constrains for some of the parameters 
+                     k = 0))
+  # control = list(warnOnly = TRUE)
+  
+  model_y <- total %>%
+    filter(Time != 1440 & Time != 1800) %>%
+    filter(Well == i) %>%
+    nls(formula = HE ~ Xinf*(1-exp(-k*Time)), # using the Weibull function 
+                 data = . ,
+                 algorithm = "port", # add this if setting the constrains 
+                 start = list(Xinf = 50, # some guessing values of the parameters
+                              k = 0.002),
+                 lower = list(Xinf = 0, # set the constrains for some of the parameters
+                              k = 0),
+                 upper = list(Xinf = 100))
+  
+  # creat a data frame that contains 100 time points
+  
+  newdata <- data.frame(Time = seq(0, 1800, len = 100))
+  
+  newdata$fitted_x <- predict(model_x, newdata)
+  
+  newdata$fitted_y <- predict(model_y, newdata)
+  
+  ggp <- total %>%
+    filter(Well == i) %>% # pipe this to the first argument on the right side
+    # here, the first argument of ggplot is the data, otherwise, we have to type ggplot(data = .)
+    # to let it pipe to this argument
+    ggplot() +
+    geom_point(aes(x = Time,
+                   y = HE),
+               shape = 1,
+               size = 2) +
+    geom_line(data = newdata,
+              aes(x = Time,
+                  y = fitted_x),
+              color = "red") +
+    geom_line(data = newdata,
+              aes(x = Time,
+                  y = fitted_y),
+              color = "blue",
+              alpha = 0.5) +
+    # geom_hline(aes(yintercept = Xinf),
+    #            linetype = "dashed",
+    #            colour = "blue") +
+    ggtitle(i) + # set the title for each plot as i 
+    scale_y_continuous(limits = c(-2,100)) + ## set the range of the y axis
+    scale_x_continuous(limits = c(0, 2000)) +
+    theme( # remove the legend
+      panel.grid = element_blank(), # remove the grid 
+      axis.line = element_line(colour = "black", size = 0.5), # add the x axis
+      panel.background = element_rect(fill = "white", color = "black"),
+      #change the backgroud color to white and the frame color to black
+      axis.ticks = element_line(colour="black", size=.5)) +
+    # change the color of the ticks into black and the size to 0.5
+    labs(x = "Time (min)", y = "Hydrolysis extent (%)") +
+    # change the title of the x and y axis
+    theme(axis.text.x = element_text(color="black", size=10), 
+          axis.text.y = element_text(color="black", size=10)) +
+    # change the color and size of the tick label for x and y axis
+    theme(plot.margin = unit(c(5.5,12,5.5,5.5), "pt"))
+  
+  print(ggp)
+  
+} 
+
+dev.off()
