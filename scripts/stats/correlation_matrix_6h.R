0561cceb269d3fa7078d10e2069ff1fb35a929e2
王雨孜 <wangyuzi@wangyuzideMacBook-Pro.local>
2020-07-02 10:11:05 +0800

    correlation matrix of h k 6h

---
+++ scripts/stats/correlation_matrix_6h.R
@@ -0,0 +1,29 @@
+# install.packages("PerformanceAnalytics")
+
+library("PerformanceAnalytics")
+
+library(tidyverse)
+
+# import the dataset
+
+df <- read_csv("analysis/fitted_weibull_parameters_6h_well_with_T0.csv")
+
+df2 <- read_csv("analysis/total_new_convert.csv") %>%
+  select(Well, Sample)
+
+df3 <- left_join(df, df2)
+
+# calculate h
+
+df3 <- df3 %>%
+  mutate(h = 1 - H) %>%
+  select(-H)
+
+#### chart.Correlation ####
+
+df_sel <- df3 %>%
+  filter(Sample != "C+" & Sample != "C-") %>%
+  select(-Well, -Sample)
+
+
+chart.Correlation(df_sel, histogram=TRUE, pch=19)
