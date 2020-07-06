1e2624f0fbb7c9da2d25c7617bb49a95a9bf71ec
王雨孜 <wangyuzi@192.168.0.108>
2020-07-07 00:13:19 +0800

    compare the variation of firstorder and weibull

--- scripts/fitting/curve_fitting_well_first-order.R
+++ scripts/fitting/curve_fitting_well_first-order.R
@@ -4,6 +4,8 @@ library(tidyverse)
 
 library(broom)
 
+#### fitting ####
+
 # read in the dataset
 
 data_15P_cal_HE_outlier_replaced <- read_csv("data/tidydata/data_15P_cal_HE_outlier_replaced.csv")
@@ -78,3 +80,65 @@ parameter_well <- bind_cols(well, parameters_with_control)
 # save the estimated parameters
 
 write_csv(parameter_well, "analysis/fitted_first-order_parameters_well_with_T0.csv")
+
+# add the well colume to the fitted parameter dataset
+
+well <- hydro_data %>% # this is the first hydro_data of this script
+  select(Well) %>% # select just the sample column
+  unique() %>% # remove the duplicated sample names
+  arrange(Well) # get the same order as the output of the split(.$Sample) step
+
+# combine the fitted parameters with their wells (by order)
+
+fitted_parameters_with_control <- bind_cols(well, parameters_with_control)
+
+# save the fitted parameters into the analysis folder 
+
+write_csv(fitted_parameters_with_control, 
+          "analysis/fitted_first-order_parameters_well_with_T0_wellnumber.csv")
+
+#### check the fitting for pos control ####
+
+df <- read_csv("data/tidydata/data_15P_cal_HE_outlier_replaced.csv") %>%
+  select(Well, Sample) %>%
+  unique()
+
+# add the sample names to the fitted parameter dataset
+
+df2 <- left_join(fitted_parameters_with_control, df)
+
+# extract the pos control
+
+pos_control <- df2 %>%
+  filter(Sample == "C+") 
+
+# calculate the std deviation for the pos control
+
+pos_control %>%
+  summarise(Mean_k = mean(k, na.rm = TRUE), 
+            Sd_k = sd(k, na.rm = TRUE),
+            Cov = Sd_k / Mean_k * 100,
+            Cov = Sd_k / Mean_k * 100,
+            Mean_Xinf = mean(Xinf, na.rm = TRUE), 
+            Sd_Xinf = sd(Xinf, na.rm = TRUE),
+            Cov_Xinf = Sd_Xinf / Mean_Xinf * 100)
+## mean_k = 0.005, sd_k = 0.0009, cov = 16.5%
+## mean_Xinf = 83.6, sd_Xinf = 3.39, cov = 4.05%
+
+#### let's compare with the k fitted by the weibull function
+
+df <- read_csv("analysis/total_new_convert.csv") %>%
+  select(Sample, k, Xinf) %>%
+  filter(Sample == "C+")
+
+# calculate the std deviation for the pos control
+
+df %>%
+  summarise(Mean_k = mean(k, na.rm = TRUE), 
+            Sd_k = sd(k, na.rm = TRUE),
+            Cov = Sd_k / Mean_k * 100,
+            Mean_Xinf = mean(Xinf, na.rm = TRUE), 
+             Sd_Xinf = sd(Xinf, na.rm = TRUE),
+             Cov_Xinf = Sd_Xinf / Mean_Xinf * 100)
+## mean_k = 0.003, sd_k = 0.002, cov = 73%
+## mean_Xinf = 82.6, sd_Xinf = 3.58, cov = 4.34%
