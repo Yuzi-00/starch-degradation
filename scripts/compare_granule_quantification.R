d21af3c7c2387c8e0665f62195320137a81e139c
王雨孜 <wangyuzi@wangyuzideMacBook-Pro.local>
2020-07-06 20:56:46 +0800

    change the y axis

--- scripts/compare_granule_quantification.R
+++ scripts/compare_granule_quantification.R
@@ -11,15 +11,12 @@ df2 <- df %>%
 
 # plot
 
-
-
-# plot
-
 p <- df2 %>%
   filter(!Sample %in% c("C+", "C-")) %>%
   ggplot(aes(x = reorder(Sample, pi_AB_ratio),
              y = pi_AB_ratio)) +
-  geom_bar(stat = 'identity') 
+  geom_bar(stat = 'identity') +
+  scale_y_continuous(limits = c(0,25), expand = c(0, 0)) 
 
 p
 
@@ -27,6 +24,7 @@ p2 <- df2 %>%
   filter(!Sample %in% c("C+", "C-")) %>%
   ggplot(aes(x = reorder(Sample, AB_ratio),
              y = AB_ratio)) +
-  geom_bar(stat = 'identity') 
+  geom_bar(stat = 'identity') +
+  scale_y_continuous(limits = c(0,25), expand = c(0, 0)) 
 
 p2
