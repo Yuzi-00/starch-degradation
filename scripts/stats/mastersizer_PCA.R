0b675f9e6d809a6dd7229bf8d2b56c4756d5bdaa
王雨孜 <wangyuzi@wangyuzideMacBook-Pro.local>
2020-09-14 23:12:05 +0200

    plot PCA with groups

--- scripts/stats/mastersizer_PCA.R
+++ scripts/stats/mastersizer_PCA.R
@@ -7,6 +7,8 @@ library(FactoMineR)
 
 library(factoextra)
 
+########################## 
+
 # import the dataset
 
 df <- read_xls("data/MastersizerMagic_forPCA.xls", sheet = "Total data")
@@ -24,3 +26,39 @@ df1 <- df %>%
   scale()
 
 res.pca <- PCA(df1, scale.unit = TRUE)
+
+eig.val <- get_eigenvalue(res.pca)
+
+eig.val
+
+# plot PCA
+
+fviz_pca_var(res.pca, col.var = "black") # we can see that there are three groups
+
+var <- get_pca_var(res.pca)
+
+var
+
+head(var$coord)
+
+# Create a grouping variable using kmeans
+# Create 3 groups of variables (centers = 3)
+
+res.km <- kmeans(var$coord, centers = 3, nstart = 25)
+
+grp <- as.factor(res.km$cluster)
+# Color variables by groups
+fviz_pca_var(res.pca, col.var = grp, 
+             palette = c("#0073C2FF", "#EFC000FF", "#868686FF"),
+             legend.title = "Cluster")
+
+# graph of individuals 
+
+ind <- get_pca_ind(res.pca)
+
+ind
+
+fviz_pca_ind(res.pca, col.ind = "cos2", 
+             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+             repel = FALSE # Avoid text overlapping (slow if many points)
+)
