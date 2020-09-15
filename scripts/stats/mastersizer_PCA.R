796606d1a3fd3d8e936470a4d1a17e0283aa797d
王雨孜 <wangyuzi@wangyuzideMacBook-Pro.local>
2020-09-16 00:06:40 +0200

    remove outliers

--- scripts/stats/mastersizer_PCA.R
+++ scripts/stats/mastersizer_PCA.R
@@ -11,17 +11,20 @@ library(factoextra)
 
 # import the dataset
 
-df <- read_xls("data/MastersizerMagic_forPCA.xls", sheet = "Total data")
+df <- read_xls("data/MastersizerMagic_forPCA.xls", sheet = "Total data") %>%
+  select(-(70:103)) # remove the aggregations (diameter > 100um)
+
 
 # remove the cav numbers
 
 df1 <- df %>%
   select(-id) %>%
   rename(Sample = 'Sample Name') %>%
+  filter(Sample != 1 & Sample != 3 & Sample != 7) %>%
   rownames_to_column() %>%
   unite("sample", rowname:Sample) %>% # I did this because duplicated rownames are not allowed 
   column_to_rownames(var = 'sample') %>%
-  select(-(1:28), -(91:103)) %>% # these colonms are all NAs or 0  
+  select(-(1:28)) %>% # these colonms are all NAs or 0  
   na.omit() %>% 
   scale()
 
@@ -44,7 +47,7 @@ head(var$coord)
 # Create a grouping variable using kmeans
 # Create 3 groups of variables (centers = 3)
 
-res.km <- kmeans(var$coord, centers = 3, nstart = 25)
+res.km <- kmeans(var$coord, centers = 2)
 
 grp <- as.factor(res.km$cluster)
 # Color variables by groups
@@ -58,7 +61,10 @@ ind <- get_pca_ind(res.pca)
 
 ind
 
-fviz_pca_ind(res.pca, col.ind = "coord", # color by coordinant
-             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+res.km <- kmeans(ind$coord, centers = 4)
+
+grp <- as.factor(res.km$cluster)
+
+fviz_pca_ind(res.pca, col.ind = grp, # color by coordinant
              repel = FALSE # Avoid text overlapping (slow if many points)
 )
